599532938f4f135b5989f6aa8cd0860a779b5cfe
Gene Calmer <yingyangforce@gmail.com>
2022-04-13 17:25:39 -0400

    reorganize, populatelist() now uses addbook(), added script section

--- grace_p1.py
+++ grace_p1.py
@@ -7,27 +7,35 @@
 # Collabs: N/a
 
 #read stock of books on init
-initlist = []
+bookdict = {}
 
-def populatelist(inlist):
-    file = open("bookStock.txt")
-    for line in file:
-        nextline = next(file)
-        inlist.append([line.strip(), int(nextline.strip())])
-
-populatelist(initlist) #populates initlist in format [title, count]
-
-#store book title, quantity in 2d list
-
-
-#add book
-def addbook():
-    pass
+def addbook(dict, title, count=1):
+    #adds count to title, adds title if not in dict
+    if title in dict:
+        dict[title] += count
+        return
+    dict.update({title: count})
+    
 
 #rem book, err if DNE
-def rembook():
+def rembook(list, title):
     pass
 
 #print stock
-def stockreport():
+def stockreport(list):
     pass
+
+def populatelist(inlist):
+    #load bookStock.txt into a dictionary on init
+    file = open("bookStock.txt")
+    for line in file:             # grab book title
+        nextline = next(file)     # grab num of books
+        addbook(inlist, line.strip(), int(nextline.strip()))
+
+
+
+# start script ---------
+
+populatelist(bookdict) #populates initlist in format [title, count]
+
+print(bookdict)
